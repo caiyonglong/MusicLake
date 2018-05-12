b581f3f273c57067f47a4ef5c7baa4ecec82a6a3
caiyonglong <caiyonglong@live.com>
2018-05-13 03:05:11 +0800

    fix:调试native调用js跨域问题

---
+++ app/src/main/java/com/cyl/musiclake/musicApi/JsApi.java
@@ -0,0 +1,23 @@
+package com.cyl.musiclake.musicApi;
+
+import android.webkit.JavascriptInterface;
+
+import wendu.dsbridge.CompletionHandler;
+
+/**
+ * Created by master on 2018/5/11.
+ */
+
+public class JsApi {
+    //同步API
+    @JavascriptInterface
+    public String testSyn(Object msg) {
+        return msg + "［syn call］";
+    }
+
+    //异步API
+    @JavascriptInterface
+    public void testAsyn(Object msg, CompletionHandler<String> handler) {
+        handler.complete(msg + " [ asyn call]");
+    }
+}
--- app/src/main/java/com/cyl/musiclake/ui/main/TestActivity.java
+++ app/src/main/java/com/cyl/musiclake/ui/main/TestActivity.java
@@ -1,21 +1,23 @@
 package com.cyl.musiclake.ui.main;
 
-import android.content.DialogInterface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.webkit.JavascriptInterface;
 import android.widget.Button;
 
 import com.cyl.musiclake.R;
 import com.cyl.musiclake.base.BaseActivity;
+import com.cyl.musiclake.musicApi.AjaxHandler;
+import com.cyl.musiclake.utils.LogUtil;
+
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import wendu.dsbridge.CompletionHandler;
+import wendu.dsbridge.DWebView;
+import wendu.dsbridge.OnReturnValue;
 
 public class TestActivity extends BaseActivity {
 
@@ -23,20 +25,16 @@ public class TestActivity extends BaseActivity {
     Button btnTest;
 
     @BindView(R.id.webview)
-    WebView mWebView;
+    DWebView mWebView;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @OnClick(R.id.btn_test)
     void test() {
-        String ooo = "dasd";
-        Log.e("tt", "data");
-        // 通过Handler发送消息
-        mWebView.post(() -> {
-
-            // 调用javascript的callJS()方法
-            mWebView.loadUrl("javascript:callJS()");
-//            mWebView.loadUrl("javascript:searchSong('周杰伦')");
-        });
+        Log.e("tt", "test");
+        mWebView.callHandler("asyn.getInfo", (OnReturnValue<JSONObject>) retValue -> {
+                    LogUtil.e(retValue.toString());
+                }
+        );
     }
 
     @Override
@@ -46,42 +44,25 @@ public class TestActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        WebSettings webSettings = mWebView.getSettings();
+        mWebView.addJavascriptObject(new Object(){
 
-        // 设置与Js交互的权限
-        webSettings.setJavaScriptEnabled(true);
-        // 设置允许JS弹窗
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setDomStorageEnabled(true);
-
-        // 先载入JS代码
-        // 格式规定为:file:///android_asset/文件名.html
-        mWebView.loadUrl("file:///android_asset/app.html");
-
-        // 由于设置了弹窗检验调用结果,所以需要支持js对话框
-        // webview只是载体，内容的渲染需要使用webviewChromClient类去实现
-        // 通过设置WebChromeClient对象处理JavaScript的对话框
-        //设置响应js 的Alert()函数
-        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-                AlertDialog.Builder b = new AlertDialog.Builder(TestActivity.this);
-                b.setTitle("Alert");
-                b.setMessage(message);
-                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                });
-                b.setCancelable(false);
-                b.create().show();
-                return true;
+            /**
+             * Note: This method is for Fly.js
+             * In browser, Ajax requests are sent by browser, but Fly can
+             * redirect requests to native, more about Fly see  https://github.com/wendux/fly
+             * @param requestData passed by fly.js, more detail reference https://wendux.github.io/dist/#/doc/flyio-en/native
+             * @param handler
+             */
+            @JavascriptInterface
+            public void onAjaxRequest(Object requestData, CompletionHandler handler){
+                LogUtil.e("TAG","-----");
+                // Handle ajax request redirected by Fly
+                AjaxHandler.onAjaxRequest((JSONObject)requestData,handler);
             }
 
-        });
-
+        },null);
+        // 格式规定为:file:///android_asset/文件名.html
+        mWebView.loadUrl("file:///android_asset/app.html");
     }
 
     @Override
